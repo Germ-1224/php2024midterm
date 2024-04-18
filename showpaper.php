3aa18f61a1a259796a73db3467a49e4a21198336
Germ-1224 <125894890+Germ-1224@users.noreply.github.com>
2024-04-18 11:36:37 +0800

    Update showpaper.php

--- showpaper.php
+++ showpaper.php
@@ -5,8 +5,8 @@ echo "論文標題：".$sTitle."<br/>";
 $sName=$_POST["sName"];
 echo "作者姓名：".$sName."<br/>";
 $sEmail=$_POST["sEmail"];
-echo "作者Email：".$sName."<br/>";
-$sComment=$_POST["sEmail"];
+echo "作者Email：".$sEmail."<br/>";
+$sComment=$_POST["sComment"];
 echo "論文摘要：".$sComment."<br/>";
 ?>
 
