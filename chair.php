63db0ef8bf872401bdb238c251957df7ce838f9f
Germ-1224 <125894890+Germ-1224@users.noreply.github.com>
2024-04-18 11:32:41 +0800

    Update chair.php

--- chair.php
+++ chair.php
@@ -8,11 +8,11 @@ session_start();
 
 if($_SESSION["check"]=="Yes"){
     echo "歡迎進入網頁</br>";
-    echo "<a href='logout_1.php'>登出</a>";
+    echo "<a href='logout.php'>登出</a>";
 }else{
     echo "非法進入網頁</br>";
     echo "3秒之後回登入頁面";
-    header("Refresh:3; url=login_1.php");
+    header("Refresh:3; url=index.php");
 }
 
 ?>
