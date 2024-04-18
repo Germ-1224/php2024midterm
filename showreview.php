37da25904e57991dda73075537cb97c30f7d4b5f
Germ-1224 <125894890+Germ-1224@users.noreply.github.com>
2024-04-18 11:37:30 +0800

    Update showreview.php

--- showreview.php
+++ showreview.php
@@ -3,7 +3,7 @@
 $decision=$_POST["decision"];
 echo "論文評審決定：".$decision."<br/>";
 $sComment=$_POST["sComment"];
-echo "你的寶貴意見：".$sComment."<br/>";
+echo "論文評論評語：".$sComment."<br/>";
 ?>
 
 <a href='logout.php'>登出</a>
